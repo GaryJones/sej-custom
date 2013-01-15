7c7b9757dcc5e092d4c6036ad65794581b300c3b
Simon Wheatley <simon@sweetinteraction.com>
2013-01-15 19:00:32 +0000

    Correct some common English UTF8 characters

--- utf8-conversion.sql
+++ utf8-conversion.sql
@@ -14,3 +14,47 @@ ALTER TABLE wp_users              CONVERT TO CHARACTER SET utf8 COLLATE utf8_gen
 
 # Referencing: https://gist.github.com/1379880
 
+update wp_posts set post_content = replace(post_content,'â€™','\'');
+update wp_posts set post_title = replace(post_title,'â€™','\'');
+update wp_comments set comment_content = replace(comment_content,'â€™','\'');
+update wp_postmeta set meta_value = replace(meta_value,'â€™','\'');
+
+update wp_posts set post_content = replace(post_content,'â€¦','...');
+update wp_posts set post_title = replace(post_title,'â€¦','...');
+update wp_comments set comment_content = replace(comment_content,'â€¦','...');
+update wp_postmeta set meta_value = replace(meta_value,'â€¦','...');
+
+update wp_posts set post_content = replace(post_content,'â€“','-');
+update wp_posts set post_title = replace(post_title,'â€“','-');
+update wp_comments set comment_content = replace(comment_content,'â€“','-');
+update wp_postmeta set meta_value = replace(meta_value,'â€“','-');
+
+update wp_posts set post_content = replace(post_content,'â€œ','"');
+update wp_posts set post_title = replace(post_title,'â€œ','"');
+update wp_comments set comment_content = replace(comment_content,'â€œ','"');
+update wp_postmeta set meta_value = replace(meta_value,'â€œ','"');
+
+update wp_posts set post_content = replace(post_content,'â€˜','\'');
+update wp_posts set post_title = replace(post_title,'â€˜','\'');
+update wp_comments set comment_content = replace(comment_content,'â€˜','\'');
+update wp_postmeta set meta_value = replace(meta_value,'â€˜','\'');
+
+update wp_posts set post_content = replace(post_content,'â€¢','-');
+update wp_posts set post_title = replace(post_title,'â€¢','-');
+update wp_comments set comment_content = replace(comment_content,'â€¢','-');
+update wp_postmeta set meta_value = replace(meta_value,'â€¢','-');
+
+update wp_posts set post_content = replace(post_content,'â€¡','c');
+update wp_posts set post_title = replace(post_title,'â€¡','c');
+update wp_comments set comment_content = replace(comment_content,'â€¡','c');
+update wp_postmeta set meta_value = replace(meta_value,'â€¡','c');
+
+update wp_posts set post_content = replace(post_content,'â€','"');
+update wp_posts set post_title = replace(post_title,'â€','"');
+update wp_comments set comment_content = replace(comment_content,'â€','"');
+update wp_postmeta set meta_value = replace(meta_value,'â€','"');
+
+update wp_posts set post_content = replace(post_content,'Â','');
+update wp_posts set post_title = replace(post_title,'Â','');
+update wp_comments set comment_content = replace(comment_content,'Â','');
+update wp_postmeta set meta_value = replace(meta_value,'Â','');
